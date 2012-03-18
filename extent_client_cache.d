93a9cd3c8368e6ca243b4b3d8561b92582fe0e8f
Owen Derby <oderby@mit.edu>
2012-03-17 22:45:55 -0400

    delete compiled files

--- extent_client_cache.d
+++
@@ -1,4 +0,0 @@
-extent_client_cache.o: extent_client_cache.cc extent_client_cache.h \
- extent_protocol.h rpc/rpc.h rpc/thr_pool.h rpc/fifo.h rpc/slock.h \
- lang/verify.h rpc/marshall.h lang/algorithm.h rpc/connection.h \
- rpc/pollmgr.h extent_client.h
